bdcb725064718dfc9b68b0b2f41456efaff40bd8
huyld <huylee.dev.vn@gmail.com>
2024-08-07 18:14:54 +0700

    create card dto

--- src/modules/card/dto/create-card.dto.ts
+++ src/modules/card/dto/create-card.dto.ts
@@ -1,1 +1,28 @@
-export class CreateCardDto {}
+import { Type } from 'class-transformer';
+import { IsArray, IsObject, ValidateNested } from 'class-validator';
+import { Category } from 'constant';
+import {
+  BooleanField,
+  EnumFieldOptional,
+  NumberField,
+  NumberFieldOptional,
+  StringField,
+  StringFieldOptional,
+  UUIDField,
+} from 'decorators';
+
+class ProductAddToCard {
+  @UUIDField()
+  productId!: Uuid;
+  @UUIDField()
+  shopId!: Uuid;
+  @NumberField()
+  quantity!: number;
+}
+
+export class CreateCardDto {
+  @IsObject()
+  @ValidateNested()
+  @Type(() => ProductAddToCard)
+  product!: ProductAddToCard;
+}
